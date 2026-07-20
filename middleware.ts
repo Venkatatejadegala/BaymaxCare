dcb0853e7a58652900c26b740017f02ef487fcbd
Degala Venkata Teja <23pa1a4227@vishnu.edu.in>
2026-07-20 18:00:24 +0530

    feat(auth): complete login with NextAuth

---
+++ middleware.ts
@@ -0,0 +1,20 @@
+import { withAuth } from 'next-auth/middleware';
+
+export default withAuth({
+  pages: {
+    signIn: '/login',
+  },
+});
+
+export const config = {
+  matcher: [
+    '/dashboard',
+    '/dashboard/:path*',
+    '/profile',
+    '/profile/:path*',
+    '/metrics',
+    '/metrics/:path*',
+    '/goals',
+    '/goals/:path*',
+  ],
+};
